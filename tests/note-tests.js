0491d69832630991deeefbd984ed63ed0d49c25e
Karsten Finlay <karstenfinlay@hotmail.com>
2021-02-16 15:13:24 +0000

    Added out first test, testing if html has a title

--- tests/note-tests.js
+++ tests/note-tests.js
@@ -1,6 +1,19 @@
+var ready = (callback) => {
+  if (document.readyState != "loading") callback();
+  else document.addEventListener("DOMContentLoaded", callback);
+}
+
 function testCircleRadiusDefaultsTo10() {
   let note = new Note();
   assert.isTrue(note.radius === 10);
 };
 
-testCircleRadiusDefaultsTo10();
+function testHomePageHasTitle() {
+  let title = document.getElementById("title")
+  assert.isTrue(title)
+}
+
+ready(() => { 
+  testCircleRadiusDefaultsTo10();
+  testHomePageHasTitle();
+});
